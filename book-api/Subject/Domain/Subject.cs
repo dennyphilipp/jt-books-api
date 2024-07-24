c3058bf7e658f59836c6f4964f56c972751c872e
dennyphilippgoncalves <denny.philipp@outlook.com>
2024-07-24 20:55:34 -0300

    Add subject domain

---
+++ book-api/Subject/Domain/Subject.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_api.Subject.Domain
+{
+    public class Subject
+    {
+        [Key]
+        [Column("Id", Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("Descricao")]
+        [MaxLength(20, ErrorMessage = "A Descrição deve ter no máximo 20 caracteres.")]
+        public string Description { get; set; }
+    }
+}
