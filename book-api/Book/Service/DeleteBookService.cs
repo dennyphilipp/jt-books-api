ed9576e59595f165c9c52fc9b7ed363148516973
dennyphilippgoncalves <denny.philipp@outlook.com>
2024-07-24 18:45:08 -0300

    Add delete book service

---
+++ book-api/Book/Service/DeleteBookService.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using book_api.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace book_api.Book.Service
+{
+    public class DeleteBookService
+    {
+        private readonly Context _context;
+
+        public DeleteBookService(Context context)
+        {
+            _context = context;
+        }
+        internal async Task Execute(int id)
+        {
+           await _context.Book.Where(b => b.Id == id)
+                              .ExecuteDeleteAsync();
+        }
+    }
+}
